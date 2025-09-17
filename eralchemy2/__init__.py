610754c2a4ee000e19c4b84d3a21f2ed8e234c29
Florian Maurer <f.maurer@outlook.de>
2025-09-17 10:59:01 +0200

    make eralchemy2 only an alias for the existing package

--- eralchemy2/__init__.py
+++ eralchemy2/__init__.py
@@ -1,3 +1,11 @@
-from .main import __version__, render_er
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "na"
+
+
+from eralchemy import render_er
 
 __all__ = ("render_er", "__version__")
